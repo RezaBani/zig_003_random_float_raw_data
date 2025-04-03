bc3755274bc214aab172ddfaf2c5be7f13ab8ab1
RezaBanivakil <rezabaniv@gmail.com>
2025-04-03 13:49:56 +0330

    0.1.0
    test moved to test.zig argument parsing improved, tests improved

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,50 @@
 const std = @import("std");
 
+pub const ArgsError = error{
+    TooManyArgs,
+};
+
+pub const ArgumentsOrder = enum(usize) {
+    // ExecutableName = 0,
+    Count = 1,
+};
+
+pub const DEFAULT_COUNT: usize = 10000;
+pub const MAX_ARGS_COUNT: usize = 2;
+
 pub fn main() !void {
-    try createFile("zig-out/bin/raw.data", 10000);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    const args = try readArgs(allocator);
+    const count = try parseArgs(args);
+    try createFile("zig-out/bin/raw.data", count);
 }
 
-fn createFile(relative_path: []const u8, count: usize) !void {
+fn readArgs(allocator: std.mem.Allocator) ![][]const u8 {
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
+    var argsArray = std.ArrayList([]u8).init(allocator);
+    while (args.next()) |arg| {
+        const buf = try allocator.alloc(u8, arg.len);
+        @memcpy(buf, arg);
+        try argsArray.append(buf);
+    }
+    return argsArray.toOwnedSlice();
+}
+
+pub fn parseArgs(args: [][]const u8) !usize {
+    if (args.len > MAX_ARGS_COUNT) {
+        std.debug.print("This executable accepts only 1 arguments but more than 1 argument is provided!\nIf you see this in test suit, it's fine\n", .{});
+        return ArgsError.TooManyArgs;
+    }
+    const count: usize = if (args.len > 1)
+        try std.fmt.parseUnsigned(usize, args[@intFromEnum(ArgumentsOrder.Count)], 10)
+    else
+        DEFAULT_COUNT;
+    return count;
+}
+
+pub fn createFile(relative_path: []const u8, count: usize) !void {
     const cwd = std.fs.cwd();
     const file = try cwd.createFile(relative_path, .{});
     defer file.close();
@@ -18,21 +58,4 @@ fn createFile(relative_path: []const u8, count: usize) !void {
         const bytes = try std.fmt.bufPrint(&buffer, "{d:7.6}", .{number});
         _ = try file.write(bytes);
     }
-}
-
-test "simple test" {
-    const cwd = std.fs.cwd();
-    const path = "zig-out/bin/test.data";
-    {
-        const count = 16000;
-        try createFile(path, count);
-        const file = try cwd.openFile(path, .{});
-        defer file.close();
-        const allocator = std.testing.allocator;
-        const content = try file.readToEndAlloc(allocator, std.math.maxInt(usize));
-        defer allocator.free(content);
-        try std.testing.expectEqual(content.len, count * 8);
-    }
-    try cwd.deleteFile(path);
-    try std.testing.expectError(std.fs.File.OpenError.FileNotFound, cwd.openFile(path, .{}));
 }
---
+++ src/test.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+
+const main = @import("main.zig");
+
+test "simple test" {
+    const cwd = std.fs.cwd();
+    const path = "zig-out/bin/test.data";
+    const allocator = std.testing.allocator;
+
+    const argumentSetOkEmpty: [][]const u8 = @constCast(&[_][]const u8{"executableName"});
+    const argumentSetOkOne: [][]const u8 = @constCast(&[_][]const u8{ "executableName", "20" });
+    const argumentSetBad: [][]const u8 = @constCast(&[_][]const u8{ "executableName", "garbage" });
+    const argumentSetBadExtra: [][]const u8 = @constCast(&[_][]const u8{ "executableName", "20", "extra" });
+
+    try std.testing.expectEqual(main.DEFAULT_COUNT, try main.parseArgs(argumentSetOkEmpty));
+    try std.testing.expectEqual(try std.fmt.parseUnsigned(usize, argumentSetOkOne[@intFromEnum(main.ArgumentsOrder.Count)], 10), try main.parseArgs(argumentSetOkOne));
+    try std.testing.expectError(std.fmt.ParseIntError.InvalidCharacter, main.parseArgs(argumentSetBad));
+    try std.testing.expectError(main.ArgsError.TooManyArgs, main.parseArgs(argumentSetBadExtra));
+    {
+        var okArgs = std.ArrayList([][]const u8).init(allocator);
+        defer okArgs.deinit();
+        try okArgs.append(argumentSetOkOne);
+        try okArgs.append(argumentSetOkEmpty);
+        for (okArgs.items) |value| {
+            const count = try main.parseArgs(value);
+            try main.createFile(path, count);
+            const file = try cwd.openFile(path, .{});
+            defer file.close();
+            const content = try file.readToEndAlloc(allocator, std.math.maxInt(usize));
+            defer allocator.free(content);
+            try std.testing.expectEqual(content.len, count * 8);
+        }
+    }
+    try cwd.deleteFile(path);
+    try std.testing.expectError(std.fs.File.OpenError.FileNotFound, cwd.openFile(path, .{}));
+}
