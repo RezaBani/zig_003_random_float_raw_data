b2fa10b1ceb0beab4d06faa03b27600ccfa1997c
RezaBanivakil <rezabaniv@gmail.com>
2025-04-03 14:51:02 +0330

    0.1.1
    error handling done in main function so main doesn't return error. parsing argument return struct instead of usize

--- src/main.zig
+++ src/main.zig
@@ -9,15 +9,52 @@ pub const ArgumentsOrder = enum(usize) {
     Count = 1,
 };
 
+pub const CommandLineArguments = struct {
+    count: usize,
+};
+
 pub const DEFAULT_COUNT: usize = 10000;
 pub const MAX_ARGS_COUNT: usize = 2;
 
-pub fn main() !void {
+pub fn main() void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    const args = try readArgs(allocator);
-    const count = try parseArgs(args);
-    try createFile("zig-out/bin/raw.data", count);
+    const rawArgs = readArgs(allocator) catch |err| switch (err) {
+        std.mem.Allocator.Error.OutOfMemory => {
+            std.debug.print("{}\n", .{err});
+            std.debug.print("Not enough emory to store args", .{});
+            std.process.exit(1);
+        },
+    };
+    const args = parseArgs(rawArgs) catch |err| switch (err) {
+        ArgsError.TooManyArgs => {
+            std.debug.print("{}\n", .{err});
+            std.debug.print("This executable accepts only {d} arguments but more than {} argument is provided!\n", .{ MAX_ARGS_COUNT - 1, MAX_ARGS_COUNT - 1 });
+            std.process.exit(1);
+        },
+        std.fmt.ParseIntError.InvalidCharacter => {
+            std.debug.print("{}\n", .{err});
+            std.debug.print("argument {s} can't be converted to unsigned interger\n", .{rawArgs[@intFromEnum(ArgumentsOrder.Count)]});
+            std.process.exit(1);
+        },
+        std.fmt.ParseIntError.Overflow => {
+            std.debug.print("{}\n", .{err});
+            std.debug.print("argument {s} too big when converted to unsigned interger\n", .{rawArgs[@intFromEnum(ArgumentsOrder.Count)]});
+            std.process.exit(1);
+        },
+    };
+    createFile("zig-out/bin/raw.data", args) catch |err| switch (err) {
+        std.fmt.BufPrintError.NoSpaceLeft => {
+            std.debug.print("{}\n", .{err});
+            std.debug.print("Buffer too small for number to fit in\n", .{});
+            std.process.exit(1);
+        },
+        else => {
+            std.debug.print("{}\n", .{err});
+            std.debug.print("error related to file operation happened", .{});
+            std.process.exit(1);
+        },
+    };
 }
 
 fn readArgs(allocator: std.mem.Allocator) ![][]const u8 {
@@ -29,22 +66,24 @@ fn readArgs(allocator: std.mem.Allocator) ![][]const u8 {
         @memcpy(buf, arg);
         try argsArray.append(buf);
     }
-    return argsArray.toOwnedSlice();
+    const slice = try argsArray.toOwnedSlice();
+    return slice;
 }
 
-pub fn parseArgs(args: [][]const u8) !usize {
+pub fn parseArgs(args: [][]const u8) !CommandLineArguments {
     if (args.len > MAX_ARGS_COUNT) {
-        std.debug.print("This executable accepts only 1 arguments but more than 1 argument is provided!\nIf you see this in test suit, it's fine\n", .{});
         return ArgsError.TooManyArgs;
     }
     const count: usize = if (args.len > 1)
         try std.fmt.parseUnsigned(usize, args[@intFromEnum(ArgumentsOrder.Count)], 10)
     else
         DEFAULT_COUNT;
-    return count;
+    return CommandLineArguments{
+        .count = count,
+    };
 }
 
-pub fn createFile(relative_path: []const u8, count: usize) !void {
+pub fn createFile(relative_path: []const u8, args: CommandLineArguments) !void {
     const cwd = std.fs.cwd();
     const file = try cwd.createFile(relative_path, .{});
     defer file.close();
@@ -53,7 +92,7 @@ pub fn createFile(relative_path: []const u8, count: usize) !void {
     const random = std.Random.DefaultPrng.random(&seed);
     var buffer: [8]u8 = undefined;
     @memset(&buffer, 0);
-    for (0..count) |_| {
+    for (0..args.count) |_| {
         const number = random.float(f64);
         const bytes = try std.fmt.bufPrint(&buffer, "{d:7.6}", .{number});
         _ = try file.write(bytes);
--- src/test.zig
+++ src/test.zig
@@ -12,8 +12,8 @@ test "simple test" {
     const argumentSetBad: [][]const u8 = @constCast(&[_][]const u8{ "executableName", "garbage" });
     const argumentSetBadExtra: [][]const u8 = @constCast(&[_][]const u8{ "executableName", "20", "extra" });
 
-    try std.testing.expectEqual(main.DEFAULT_COUNT, try main.parseArgs(argumentSetOkEmpty));
-    try std.testing.expectEqual(try std.fmt.parseUnsigned(usize, argumentSetOkOne[@intFromEnum(main.ArgumentsOrder.Count)], 10), try main.parseArgs(argumentSetOkOne));
+    try std.testing.expectEqual(main.DEFAULT_COUNT, (try main.parseArgs(argumentSetOkEmpty)).count);
+    try std.testing.expectEqual(try std.fmt.parseUnsigned(usize, argumentSetOkOne[@intFromEnum(main.ArgumentsOrder.Count)], 10), (try main.parseArgs(argumentSetOkOne)).count);
     try std.testing.expectError(std.fmt.ParseIntError.InvalidCharacter, main.parseArgs(argumentSetBad));
     try std.testing.expectError(main.ArgsError.TooManyArgs, main.parseArgs(argumentSetBadExtra));
     {
@@ -21,14 +21,14 @@ test "simple test" {
         defer okArgs.deinit();
         try okArgs.append(argumentSetOkOne);
         try okArgs.append(argumentSetOkEmpty);
-        for (okArgs.items) |value| {
-            const count = try main.parseArgs(value);
-            try main.createFile(path, count);
+        for (okArgs.items) |rawArgs| {
+            const args = try main.parseArgs(rawArgs);
+            try main.createFile(path, args);
             const file = try cwd.openFile(path, .{});
             defer file.close();
             const content = try file.readToEndAlloc(allocator, std.math.maxInt(usize));
             defer allocator.free(content);
-            try std.testing.expectEqual(content.len, count * 8);
+            try std.testing.expectEqual(content.len, args.count * 8);
         }
     }
     try cwd.deleteFile(path);
